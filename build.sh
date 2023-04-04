10ba35a5604191b3cd917a14b88a336893de0ba9
zyxkad <zyxkad@gmail.com>
2023-04-03 21:47:33 -0600

    add ASYNC_BUILD

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 cd $(dirname $0)
 
+echo
+go version || exit $?
+echo
+
 export CGO_ENABLED=0 # cross-compile without cgo
 
 function _build(){
@@ -17,16 +21,81 @@ function _build(){
 arch32=(386 arm)
 arch64=(amd64 arm64)
 
+if [ "$ASYNC_BUILD" = 0 ] || [ "$ASYNC_BUILD" = false ]; then
+	ASYNC_BUILD=
+else
+	echo ">>> Making logs dir 'logs.tmp'"
+	mkdir -p 'logs.tmp'
+	[ -f .flag.error ] && rm .flag.error
+fi
+
 for arch in "${arch32[@]}" "${arch64[@]}"; do
-	GOOS=linux GOARCH=$arch _build || exit $?
+	if [ -n "$ASYNC_BUILD" ]; then
+		logf="$(pwd)/logs.tmp/linux-${arch}.log"
+		{
+			if ! GOOS=linux GOARCH=$arch _build; then
+				touch .flag.error
+				exit 1
+			fi
+			echo "[+++] Done" >&2
+		} 1>"$logf" &
+	else
+		GOOS=linux GOARCH=$arch _build || exit $?
+	fi
 done
 
 for arch in "${arch32[@]}" "${arch64[@]}"; do
-	GOOS=windows GOARCH=$arch _build || exit $?
+	if [ -n "$ASYNC_BUILD" ]; then
+		logf="$(pwd)/logs.tmp/windows-${arch}.log"
+		{
+			if ! GOOS=windows GOARCH=$arch _build; then
+				touch .flag.error
+				exit 1
+			fi
+			echo "[+++] Done" >&2
+		} 1>"$logf" &
+	else
+		GOOS=windows GOARCH=$arch _build || exit $?
+	fi
 done
 
 for arch in "${arch64[@]}"; do
-	GOOS=darwin GOARCH=$arch _build || exit $?
+	if [ -n "$ASYNC_BUILD" ]; then
+		logf="$(pwd)/logs.tmp/darwin-${arch}.log"
+		{
+			if ! GOOS=darwin GOARCH=$arch _build; then
+				touch .flag.error
+				exit 1
+			fi
+			echo "[+++] Done" >&2
+		} 1>"$logf" &
+	else
+		GOOS=darwin GOARCH=$arch _build || exit $?
+	fi
 done
+
+if [ -n "$ASYNC_BUILD" ]; then
+	echo ">>> waiting works..."
+	wait
+	if [ -f .flag.error ]; then
+		rm .flag.error
+		echo "Some error have been happend, please check the console and the logs"
+		if [ -n "$CI" ]; then # if it's in github action
+			for log in `(ls logs.tmp/*.log)`; do
+				echo
+				echo "================START ${log}================"
+				cat "$log"
+				echo "================END ${log}================"
+				echo
+			done
+		fi
+		exit 1
+	fi
+fi
+
+if [ -n "$ASYNC_BUILD" ] && ! [ -n "$KEEP_TMP_LOGS" ]; then
+	echo ">>> Clearing logs at 'logs.tmp'"
+	rm -rf logs.tmp
+fi
 
 echo "==> Done"
