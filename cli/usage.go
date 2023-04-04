93981b872a04a5ef81912b1d748034febf9ff020
zyxkad <zyxkad@gmail.com>
2023-04-04 12:18:38 -0600

    update READMEs

--- cli/usage.go
+++ cli/usage.go
@@ -9,6 +9,6 @@ const UsageText =
         Install minecraft 1.19.2 forge server into current directory and the executable is minecraft_server.sh
         Hint: forge installer will make run scripts for the minecraft version that higher or equal than 1.17
               for version that less than 1.17, you still need to use 'java -jar' to run the server
-  minecraft_installer -name minecraft_server -version 1.19.2 -server fabric -path server
+  minecraft_installer -name minecraft_server -version 1.19.2 -server fabric -output server
         Install minecraft 1.19.2 fabric server into server/minecraft_server.jar
 `
